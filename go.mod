4da362c8aa6a76d51eab860370efd1ff90f7c045
qiusnay <qiusnay@gmail.com>
2020-09-14 17:59:16 +0800

    first commit for go project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gocron
+
+go 1.12
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/qiusnay/gocron/config"
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	config.LoadConfig() // 读取DB配置
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+	fmt.Println("hello world")
+}
